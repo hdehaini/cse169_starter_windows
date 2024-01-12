12f463db3073753411507c21b93f92cc9cd968b1
hsdehaini <99140276+hsdehaini@users.noreply.github.com>
2024-01-12 12:08:41 -0800

    Starter code

---
+++ build/CMakeFiles/menv.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/hsdeh/OneDrive/Documents/Coding-Projects/UCSD/CSE-169/cse169_starter_windows/main.cpp" "CMakeFiles/menv.dir/main.cpp.obj" "gcc" "CMakeFiles/menv.dir/main.cpp.obj.d"
+  "C:/Users/hsdeh/OneDrive/Documents/Coding-Projects/UCSD/CSE-169/cse169_starter_windows/src/Camera.cpp" "CMakeFiles/menv.dir/src/Camera.cpp.obj" "gcc" "CMakeFiles/menv.dir/src/Camera.cpp.obj.d"
+  "C:/Users/hsdeh/OneDrive/Documents/Coding-Projects/UCSD/CSE-169/cse169_starter_windows/src/Cube.cpp" "CMakeFiles/menv.dir/src/Cube.cpp.obj" "gcc" "CMakeFiles/menv.dir/src/Cube.cpp.obj.d"
+  "C:/Users/hsdeh/OneDrive/Documents/Coding-Projects/UCSD/CSE-169/cse169_starter_windows/src/Shader.cpp" "CMakeFiles/menv.dir/src/Shader.cpp.obj" "gcc" "CMakeFiles/menv.dir/src/Shader.cpp.obj.d"
+  "C:/Users/hsdeh/OneDrive/Documents/Coding-Projects/UCSD/CSE-169/cse169_starter_windows/src/Tokenizer.cpp" "CMakeFiles/menv.dir/src/Tokenizer.cpp.obj" "gcc" "CMakeFiles/menv.dir/src/Tokenizer.cpp.obj.d"
+  "C:/Users/hsdeh/OneDrive/Documents/Coding-Projects/UCSD/CSE-169/cse169_starter_windows/src/Window.cpp" "CMakeFiles/menv.dir/src/Window.cpp.obj" "gcc" "CMakeFiles/menv.dir/src/Window.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/menv.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for menv.
+# This may be replaced when dependencies are built.
